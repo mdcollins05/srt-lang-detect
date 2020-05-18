21682378808a526d887b6f8201297ef793238711
MDCollins <matt@m-collins.com>
2020-05-17 18:34:37 -0700

    Don't change language from 2 <-> 3 characters, unless requested

--- srtlangdetect.py
+++ srtlangdetect.py
@@ -83,6 +83,10 @@ def lang_detect_srt(file, summary, dry_run, quiet, verbose, args):
     new_language = sub_detection_results[0][0]
     new_language_confidence = sub_detection_results[0][1]
 
+    # Try not to change the language in the filename if we can avoid it
+    if file_language != "Unknown":
+        new_language = file_language
+
     if args.two_letter:
         new_language = to_2_letter_lang(new_language)
     elif args.three_letter:
