a69cedb0f40f20261eec1230f45f951c7b7cc9d3
MDCollins <matt@m-collins.com>
2024-07-14 21:41:21 -0700

    Adjust some default values

--- srtlangdetect.py
+++ srtlangdetect.py
@@ -232,22 +232,25 @@ def parse_args():
         "-c",
         default=50,
         type=check_valid_percentage,
-        help="Require a confidence percentage equal or higher than the provided value to delete or rename a file based on language(default 50) (valid range 1-100)",
+        help="Require a confidence percentage equal or higher than the provided value to delete or rename a file based on language (default 50) (valid range 1-100)",
     )
     argsparser.add_argument(
         "--min-sdh-confidence",
-        default=15,
-        help="Minimum SDH confidence to consider a file as SDH (default 15)",
+        default=5,
+        type=check_valid_percentage,
+        help="Minimum SDH confidence to consider a file as SDH (default 5) (valid range 1-100)",
     )
     argsparser.add_argument(
         "--max-sdh-confidence",
         default=85,
-        help="Maximum SDH confidence to consider a file as SDH (default 85)",
+        type=check_valid_percentage,
+        help="Maximum SDH confidence to consider a file as SDH (default 85) (valid range 1-100)",
     )
     argsparser.add_argument(
         "--reject-sdh-confidence",
-        default=5,
-        help="Reject SDH confidence to remove SDH flag (default 5)",
+        default=2,
+        type=check_valid_percentage,
+        help="Reject SDH confidence to remove SDH flag (default 2) (valid range 1-100)",
     )
     two_three_group = argsparser.add_mutually_exclusive_group()
     two_three_group.add_argument(
@@ -398,7 +401,7 @@ def get_new_filename(full_path, language, file_language, special, forced, verbos
     return new_filename
 
 def percent_sdh(input_text):
-    sdh_regex = re.compile(r"\[.*\]")
+    sdh_regex = re.compile(r"(\[.*\]|<.*>|\(.*\))")
 
     # Remove empty lines
     input_text = re.sub(r"\n\s*\n", "\n", input_text)
